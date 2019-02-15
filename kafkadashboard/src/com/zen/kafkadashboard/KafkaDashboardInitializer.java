331ac10cc091fa64179a12feca0c2ca215f61e6a
mnimbalk <mnimbalk@MNIMBALK-CY3AF.partnet.cisco.com>
2019-02-15 15:43:18 +0530

    Initial import.

---
+++ kafkadashboard/src/com/zen/kafkadashboard/KafkaDashboardInitializer.java
@@ -0,0 +1,20 @@
+package com.zen.kafkadashboard;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class KafkaDashboardInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] { KafkaDashboardConfig.class };
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+}
